9e0129ee186452d2a2c9b1b2d56408ab8cdf3b38
siqizheng <cn.siqizheng@gmail.com>
2023-12-08 13:30:03 +0100

    transform char list with new char to DetailCard

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -3,6 +3,8 @@ import {Link} from "react-router-dom";
 export default function Navigation(){
     return (
         <>
+            <Link to={"/"}>Home</Link>
+            <span> </span>
             <Link to="/characters">Characters</Link>
             <span> </span>
             <Link to={"/addnewcharacter"}>Admin</Link>
