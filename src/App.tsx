6059a3d527f03bba165f296f7ec45596fcb30664
siqizheng <cn.siqizheng@gmail.com>
2023-12-08 15:59:20 +0100

    fetch the data from API-Web

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import React, {useEffect, useState} from "react";
-import {Character, charactersResponse} from "./characters.ts";
 import CharacterGallery from "./CharacterGallery.tsx";
 import {Link, Navigate, Route, Routes, useParams} from "react-router-dom";
 import Welcome from "./Welcome.tsx";
@@ -8,16 +7,36 @@ import Navigation from "./Navigation.tsx";
 import CharacterCard from "./CharacterCard.tsx";
 import CharacterDetailCard from "./CharacterDetailCard.tsx";
 import AddNewCharacter from "./addNewCharacter.tsx";
+import axios from "axios";
 
 function App() {
+    type Character = {
+        id: number
+        name: string
+        status: string
+        species: string
+        type: string
+        image: string
+    }
+    const [characters, setCharacters] = useState<Character[]>(
+     [{id: 0,name: "",status: "",species: "",type: "",image: ""}])
 
-    const [characters, setCharacters] = useState<Character[]>(charactersResponse.results)
 
     function addNewCharacterInList(newCharacter:Character){
         setCharacters([...characters,newCharacter])
     }
 
+    const fetchData = ()=>{
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((response)=>setCharacters(response.data.results))
+            .catch(error=>{
+                console.error("Error information:",error)
+            })
+    }
 
+    useEffect(
+        ()=>{fetchData()}, []
+    )
 
     return (
         <>
--- src/CharacterGallery.tsx
+++ src/CharacterGallery.tsx
@@ -1,14 +1,22 @@
 import {Character} from "./characters.ts";
 import CharacterCard from "./CharacterCard.tsx";
+import axios from "axios";
 
 type CharacterGalleryProps = {
     characters: Character[]
 }
 
+// const fetchData = ()=>{
+//     axios.get("https://rickandmortyapi.com/api/character")
+//         .then((response)=>console.log(response.data.results)
+//         )
+// }
+
 export default function CharacterGallery(props: CharacterGalleryProps) {
 
     return (
         <div>
+            {/*<button onClick={fetchData}>click</button>*/}
             <h2>
                 ---Gallery---
             </h2>
